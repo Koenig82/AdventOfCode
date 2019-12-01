f7ab59eb4f34c165067b6731e513963ce90b9405
koenig <konigsson1982@gmail.com>
2019-12-01 21:16:51 +0100

    Identify duplicates 1 iteration

--- src/y2018/Day05.java
+++ src/y2018/Day05.java
@@ -4,11 +4,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 import adventOfCode.AdventOfCode;
 
@@ -21,7 +23,6 @@ public class Day05 extends AdventOfCode{
 	@Override
 	public void part1() throws Exception {
 		char[] input = getInput();
-		//char[] input = {'a','b','c','A','a','c','t'};
 		for(int i = 0; i < input.length-1; i++) {
 			if((int)input[i] == (int)input[i+1]-32) {
 				System.out.println("Char at index "+i+" = "+input[i]+", Char at index "+(i+1)+" = "+input[i+1]);
@@ -39,8 +40,17 @@ public class Day05 extends AdventOfCode{
 
 	private char[] getInput() {
 		
-		File file = new File("src/y2018/Day05Input.txt");
-		String string = file.toString();
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		try (Stream<String> stream = Files.lines( Paths.get("src/y2018/testinput.txt"), StandardCharsets.UTF_8)) {
+        //try (Stream<String> stream = Files.lines( Paths.get("src/y2018/Day05Input.txt"), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> stringBuilder.append(s).append("\n"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }      
+        String string = stringBuilder.toString();
+		
 		return string.toCharArray();
 	}
 }
