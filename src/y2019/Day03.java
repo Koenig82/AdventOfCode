abd691c681d44cd902d615393aedf7bb8c29c5a4
konignik <konignik@WL358744.eu.tieto.com>
2019-12-03 16:27:18 +0100

    streamtest

--- src/y2019/Day03.java
+++ src/y2019/Day03.java
@@ -1,8 +1,30 @@
 package y2019;
 
-public class Day03 {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import adventOfCode.AdventOfCode;
+
+public class Day03 extends AdventOfCode {
 
 	public static void main(String[] args) {
+		new Day03().run();
+	}
+
+	@Override
+	public void part1() throws Exception {
+		List<Integer> list = Arrays.asList(5, 3, 4, 17, 6, 10, 29, 33, 456,
+				231);
+		Map<Integer, Character> col = list.stream()
+				.collect(Collectors.toMap(e -> e, e -> (char) e.intValue()));
+
+		System.out.println(col);
+	}
+
+	@Override
+	public void part2() throws Exception {
 		// TODO Auto-generated method stub
 
 	}
