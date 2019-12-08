0d98ac430a38695f06373f7d6efec1327a59ab21
koenig <konigsson1982@gmail.com>
2019-12-08 20:12:36 +0100

    day3

--- src/y2019/Day03.java
+++ src/y2019/Day03.java
@@ -5,13 +5,13 @@ import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 import adventOfCode.AdventOfCode;
 import adventOfCode.Pair;
@@ -26,20 +26,19 @@ public class Day03 extends AdventOfCode {
 	public void part1() throws Exception {
 		List<Wire> wires = getInput();
 		WireGrid grid = new WireGrid(wires);
-		System.out.println("Result = "+grid.intersectionDistances.first());
-		//System.out.println(grid.intersectionDistances.size());
-	
+		System.out.println("Result = "+grid.getClosestIntersection());
 	}
 
 	@Override
 	public void part2() throws Exception {
-		// TODO Auto-generated method stub
+		List<Wire> wires = getInput();
+		WireGrid grid = new WireGrid(wires);
+		System.out.println("Result = "+grid.getBestIntersection());
 
 	}
 
 	private List<Wire> getInput() throws UnsupportedEncodingException, IOException {
 		
-		//List<String> readAllLines = Files.readAllLines(Paths.get("src/y2019/testinput"));
 		List<String> readAllLines = Files.readAllLines(Paths.get("src/y2019/day03Input.txt"));
 		List<Wire> wires = new ArrayList<>();
 		
@@ -51,60 +50,231 @@ public class Day03 extends AdventOfCode {
 	}
 	
 	private class WireGrid{
-		int posX;
-		int posY;
 		
-		Set<Pair<Integer,Integer>> wirePositions;
-		TreeSet<Integer> intersectionDistances;
-
+		List<Wire> wires;
+		
 		public WireGrid(List<Wire> wires) {
-			wirePositions = new HashSet<>();
-			intersectionDistances = new TreeSet<>();
-			for (Wire wire : wires) {
-				posX = 0;
-				posY = 0;
-				for (Instruction instruction : wire.instructions) {
-					switch (instruction.dir) {
-					case 'R':
-						for(int i = 0; i < instruction.length; i++) {
-							posX++;
-							if(!wirePositions.add(new Pair<Integer,Integer>(posX,posY))) {
-								intersectionDistances.add(Math.abs(posX)+Math.abs(posY));
-							}
-						}
-						break;
-					case 'L':
-						for(int i = 0; i < instruction.length; i++) {
-							posX--;
-							if(!wirePositions.add(new Pair<Integer,Integer>(posX,posY))) {
-								intersectionDistances.add(Math.abs(posX)+Math.abs(posY));
-							}
-						}
-						break;
-					case 'D':
-						for(int i = 0; i < instruction.length; i++) {
-							posY--;
-							if(!wirePositions.add(new Pair<Integer,Integer>(posX,posY))) {
-								intersectionDistances.add(Math.abs(posX)+Math.abs(posY));
-							}
-						}
-						break;
-					case 'U':
-						for(int i = 0; i < instruction.length; i++) {
-							posY++;
-							if(!wirePositions.add(new Pair<Integer,Integer>(posX,posY))) {
-								intersectionDistances.add(Math.abs(posX)+Math.abs(posY));
-							}
-						}
-						break;
-
-					default:
-						throw new IllegalArgumentException(
-								"Unexpected value: " + instruction.dir);
+			this.wires = wires;			
+		}
+		public int getClosestIntersection() {
+			Set<Pair<Integer,Integer>> wirePositions = new HashSet<>();
+			TreeSet<Integer> intersectionDistances = new TreeSet<>();
+			int posX;
+			int posY;
+			posX = 0;
+			posY = 0;
+			for (Instruction instruction : wires.get(0).instructions) {
+				switch (instruction.dir) {
+				case 'R':
+					for(int i = 0; i < instruction.length; i++) {
+						posX++;
+						wirePositions.add(new Pair<Integer,Integer>(posX,posY));
 					}
+					break;
+				case 'L':
+					for(int i = 0; i < instruction.length; i++) {
+						posX--;
+						wirePositions.add(new Pair<Integer,Integer>(posX,posY));
+					}
+					break;
+				case 'D':
+					for(int i = 0; i < instruction.length; i++) {
+						posY--;
+						wirePositions.add(new Pair<Integer,Integer>(posX,posY));
+					}
+					break;
+				case 'U':
+					for(int i = 0; i < instruction.length; i++) {
+						posY++;
+						wirePositions.add(new Pair<Integer,Integer>(posX,posY));
+					}
+					break;
+
+				default:
+					throw new IllegalArgumentException(
+							"Unexpected value: " + instruction.dir);
 				}
 			}
+			posX = 0;
+			posY = 0;
+			for (Instruction instruction : wires.get(1).instructions) {
+				switch (instruction.dir) {
+				case 'R':
+					for(int i = 0; i < instruction.length; i++) {
+						posX++;
+						if(!wirePositions.add(new Pair<Integer,Integer>(posX,posY))) {
+							intersectionDistances.add(Math.abs(posX)+Math.abs(posY));
+						}
+					}
+					break;
+				case 'L':
+					for(int i = 0; i < instruction.length; i++) {
+						posX--;
+						if(!wirePositions.add(new Pair<Integer,Integer>(posX,posY))) {
+							intersectionDistances.add(Math.abs(posX)+Math.abs(posY));
+						}
+					}
+					break;
+				case 'D':
+					for(int i = 0; i < instruction.length; i++) {
+						posY--;
+						if(!wirePositions.add(new Pair<Integer,Integer>(posX,posY))) {
+							intersectionDistances.add(Math.abs(posX)+Math.abs(posY));
+						}
+					}
+					break;
+				case 'U':
+					for(int i = 0; i < instruction.length; i++) {
+						posY++;
+						if(!wirePositions.add(new Pair<Integer,Integer>(posX,posY))) {
+							intersectionDistances.add(Math.abs(posX)+Math.abs(posY));
+						}
+					}
+					break;
+
+				default:
+					throw new IllegalArgumentException(
+							"Unexpected value: " + instruction.dir);
+				}
+			}
+			return intersectionDistances.first();
 		}
+		public int getBestIntersection() {
+			HashMap<Pair<Integer,Integer>,Integer> positions = new HashMap<>();
+			HashMap<Pair<Integer,Integer>,Integer> intersections = new HashMap<>();
+			int posX;
+			int posY;
+			int steps;
+			Pair<Integer,Integer> position;
+
+			posX = 0;
+			posY = 0;
+			steps = 0;
+			for (Instruction instruction : wires.get(0).instructions) {
+				switch (instruction.dir) {
+				case 'R':
+					for(int i = 0; i < instruction.length; i++) {
+						steps++;
+						posX++;
+						position = new Pair<Integer,Integer>(posX,posY);
+						if(!positions.containsKey(position)) {
+							positions.put(position,steps);
+						}
+					}
+					break;
+				case 'L':
+					for(int i = 0; i < instruction.length; i++) {
+						steps++;
+						posX--;
+						position = new Pair<Integer,Integer>(posX,posY);
+						if(!positions.containsKey(position)) {
+							positions.put(position,steps);
+						}
+					}
+					break;
+				case 'D':
+					for(int i = 0; i < instruction.length; i++) {
+						steps++;
+						posY--;
+						position = new Pair<Integer,Integer>(posX,posY);
+						if(!positions.containsKey(position)) {
+							positions.put(position,steps);
+						}
+					}
+					break;
+				case 'U':
+					for(int i = 0; i < instruction.length; i++) {
+						steps++;
+						posY++;
+						position = new Pair<Integer,Integer>(posX,posY);
+						if(!positions.containsKey(position)) {
+							positions.put(position,steps);
+						}
+					}
+					break;
+
+				default:
+					throw new IllegalArgumentException(
+							"Unexpected value: " + instruction.dir);
+				}
+			}
+			posX = 0;
+			posY = 0;
+			steps = 0;
+			for (Instruction instruction : wires.get(1).instructions) {
+				switch (instruction.dir) {
+				case 'R':
+					for(int i = 0; i < instruction.length; i++) {
+						steps++;
+						posX++;
+						position = new Pair<Integer,Integer>(posX,posY);
+						if(positions.containsKey(position)) {
+							if(intersections.containsKey(position)) {
+								intersections.put(position, intersections.get(position)+steps);
+							}else {
+								intersections.put(position, positions.get(position)+steps);
+							}
+						}
+					}
+					break;
+				case 'L':
+					for(int i = 0; i < instruction.length; i++) {
+						steps++;
+						posX--;
+						position = new Pair<Integer,Integer>(posX,posY);
+						if(positions.containsKey(position)) {
+							if(intersections.containsKey(position)) {
+								intersections.put(position, intersections.get(position)+steps);
+							}else {
+								intersections.put(position, positions.get(position)+steps);
+							}
+						}
+					}
+					break;
+				case 'D':
+					for(int i = 0; i < instruction.length; i++) {
+						steps++;
+						posY--;
+						position = new Pair<Integer,Integer>(posX,posY);
+						if(positions.containsKey(position)) {
+							if(intersections.containsKey(position)) {
+								intersections.put(position, intersections.get(position)+steps);
+							}else {
+								intersections.put(position, positions.get(position)+steps);
+							}
+						}
+					}
+					break;
+				case 'U':
+					for(int i = 0; i < instruction.length; i++) {
+						steps++;
+						posY++;
+						position = new Pair<Integer,Integer>(posX,posY);
+						if(positions.containsKey(position)) {
+							if(intersections.containsKey(position)) {
+								intersections.put(position, intersections.get(position)+steps);
+							}else {
+								intersections.put(position, positions.get(position)+steps);
+							}
+						}
+					}
+					break;
+
+				default:
+					throw new IllegalArgumentException(
+							"Unexpected value: " + instruction.dir);
+				}
+			}
+
+			int tempvalue = Integer.MAX_VALUE;
+			for (Integer value : intersections.values()) {
+				if(value < tempvalue) {
+					tempvalue = value;
+				}
+			}
+			return tempvalue;
+		}
+		
 	}
 
 	private class Wire{
