ae93b77c7e14bbe07f43dc81fc758125579644cd
koenig <konigsson1982@gmail.com>
2019-12-08 21:39:24 +0100

    part 1 done

--- src/y2019/Day04.java
+++ src/y2019/Day04.java
@@ -12,14 +12,12 @@ public class Day04 extends AdventOfCode{
 	@Override
 	public void part1() throws Exception {
 		int possibilities = 0;
-		boolean hasDouble;
-		boolean isAscending;
-		for(int i = 158126; i <= 624574; i++) {
+		for(int i = 158127; i < 624576; i++) {
 			if(checkNumber(i)) {
 				possibilities++;
 			}
 		}
-		System.out.println(possibilities);	
+		System.out.println("Result = "+possibilities);	
 	}
 
 	@Override
@@ -33,7 +31,7 @@ public class Day04 extends AdventOfCode{
 		boolean hasDouble = false;
 		for(int symbolIndex = 0; symbolIndex < 5;symbolIndex++) {
 			
-			if(Integer.parseInt(number.substring(symbolIndex, symbolIndex+1)) < Integer.parseInt(number.substring(symbolIndex+1, symbolIndex+2))){
+			if(Integer.parseInt(number.substring(symbolIndex, symbolIndex+1)) > Integer.parseInt(number.substring(symbolIndex+1, symbolIndex+2))){
 				return false;
 			}
 			if(Integer.parseInt(number.substring(symbolIndex, symbolIndex+1)) == Integer.parseInt(number.substring(symbolIndex+1, symbolIndex+2))) {
