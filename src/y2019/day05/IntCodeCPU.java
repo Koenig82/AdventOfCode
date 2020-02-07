1de30c84a12b99351bbb9394fb27e53ec4d9d18f
konignik <konignik@WL358744.eu.tieto.com>
2020-02-07 15:42:17 +0100

    ready to start with parametermode

--- src/y2019/day05/IntCodeCPU.java
+++ src/y2019/day05/IntCodeCPU.java
@@ -5,21 +5,25 @@ import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayDeque;
+import java.util.Scanner;
 
 public class IntCodeCPU {
 	
 	private ArrayDeque<Integer> input = new ArrayDeque<>();
+	private Scanner scanner;
 	public int[] memory;
 	//private ParameterMode parameterModes;
 
 	public IntCodeCPU() {
 		//parameterModes = new ParameterMode();
+		scanner = new Scanner(System.in);
 	}
 
 	public void loadProgram() throws UnsupportedEncodingException, IOException {
 
 		//String instructions = new String(Files.readAllBytes(Paths.get("src/y2019/day05/testinput2")), "UTF-8");
-		String instructions = new String(Files.readAllBytes(Paths.get("src/y2019/day05/testinput")), "UTF-8");
+		//String instructions = new String(Files.readAllBytes(Paths.get("src/y2019/day05/testinput")), "UTF-8");
+		String instructions = new String(Files.readAllBytes(Paths.get("src/y2019/day05/testinput3")), "UTF-8");
 		//String instructions = new String(Files.readAllBytes(Paths.get("src/y2019/day02/day02Input.txt")), "UTF-8");
 		//String input = new String(Files.readAllBytes(Paths.get("src/y2019/day05Input.txt")), "UTF-8");
 		String[] substrings = instructions.split(",");
@@ -31,14 +35,12 @@ public class IntCodeCPU {
 		this.memory = intArray;
 	}
 	
-	public void getInput(int head, Mode parameterMode) {
+	public int getData(int index, Mode parameterMode) {
 		switch (parameterMode) {
 		case position:
-			this.input.add(memory[head]);
-			break;
+			return memory[index];
 		case intermidiate:
-			this.input.add(head);
-			break;
+			return index;
 		default:
 			throw new IllegalArgumentException("Unexpected value: " + parameterMode);
 		}
@@ -48,14 +50,17 @@ public class IntCodeCPU {
 	public int executeProgram() {
 		for(int head = 0;head < memory.length;) {
 			displayMemory();
+			//input.add(scanner.nextInt());
 			if(memory[head] == 1) {
 				System.out.println("opcode 1 because value "+memory[head]+" was read index: "+head);
 				head = opcode1(head);
 			}else if(memory[head] == 2) {
 				System.out.println("opcode 2 because value "+memory[head]+" was read index: "+head);
 				head = opcode2(head);
-			//}else if(memory[head] == 3) {
-				//head = opcode3(memory, head);
+			}else if(memory[head] == 3) {
+				head = opcode3(head);
+			}else if(memory[head] == 4) {
+				head = opcode4(head);
 			}else if(memory[head] == 99){
 				break;
 			}else {
@@ -102,46 +107,45 @@ public class IntCodeCPU {
 
 	private int opcode1(int head) {
 
-		getInput(memory[head+1], Mode.position);
-		getInput(memory[head+2], Mode.position);
-		int a = input.pop();
-		int b = input.pop();
+		int a = getData(memory[head+1], Mode.position);
+		int b = getData(memory[head+2], Mode.position);
 		
 		System.out.println("writing "+(a+b)+" to memory at index "+ memory[head+3]);
 		System.out.println(a+" "+b+" read from indexes "+(head+1)+" and "+(head+2));
 		System.out.println();
 		writeToMemory(a+b, memory[head+3]);
-		
-		//int c = input.pop();
-		
-		//System.out.println(a+" "+b);
-		
-		//writeToMemory(a+b, c);
-		//memory[memory[head+3]] = memory[memory[head+1]] + memory[memory[head+2]];
-		//int[] parameters = readParameter(head, 1);
 
-		//machineCode[machineCode[head+3]] = parameters[0] + parameters[1];
 		return head+4;
 	}
+	
 	private int opcode2(int head) {
 
-		getInput(memory[head+1], Mode.position);
-		getInput(memory[head+2], Mode.position);
-		int a = input.pop();
-		int b = input.pop();
+		int a = getData(memory[head+1], Mode.position);
+		int b = getData(memory[head+2], Mode.position);
 		
 		System.out.println("writing "+(a*b)+" to memory at index "+ memory[head+3]);
 		System.out.println(a+" "+b+" read from indexes "+(head+1)+" and "+(head+2));
 		System.out.println();
 		writeToMemory(a*b, memory[head+3]);
 		
-		//System.out.println(a+" "+b);
-		
 		return head+4;
 	}
-	private int opcode3(int[] array, int index) {
-		array[array[index+1]] = array[index+1];
-		return index;
+	
+	private int opcode3(int head) {
+		int input = scanner.nextInt();
+		System.out.println("writing "+(input)+" to memory at index "+ memory[head+1]);
+		System.out.println();
+		writeToMemory(input, memory[head+1]);
+		
+		return head+1;
+	}
+	
+	private int opcode4(int head) {
+		int value = getData(memory[head+1], Mode.position);
+		System.out.println("OUTPUT: "+value);
+		System.out.println();
+		
+		return head+1;
 	}
 	
 	public void writeToMemory(int symbol, int index) {
