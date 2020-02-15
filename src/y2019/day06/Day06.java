b348e39fba92a51f9db970a9316fb4041a041cb9
koenig <konigsson1982@gmail.com>
2020-02-15 23:08:43 +0100

    asdga

--- src/y2019/day06/Day06.java
+++ src/y2019/day06/Day06.java
@@ -3,7 +3,7 @@ package y2019.day06;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Set;
+import java.util.HashSet;
 import java.util.stream.Stream;
 
 import adventOfCode.AdventOfCode;
@@ -16,10 +16,29 @@ public class Day06 extends AdventOfCode{
 
 	@Override
 	public void part1() throws Exception {
-		Set<String> input = getInput();
+		HashSet<String> input = getInput();
 		CelestialBody com = new CelestialBody("COM");
+		String first = "COM";
+		String body = "COM";
+		for (String string : input) {
+			if(string.startsWith(first)) {
+				String[] substrings = string.split("\\)");
+				com.inOrbit.add(new CelestialBody(substrings[1]));
+				body = substrings[1];
+			}
+		}
+		input.remove(first);
+		while(!input.isEmpty()) {
+			for (String string : input) {
+				if(string.startsWith(body)) {
+					String[] substrings = string.split("\\)");
+					com.inOrbit.add(new CelestialBody(substrings[1]));
+				}
+			}
+			input.remove(body);
+			
+		}
 		System.out.println(input);
-		
 	}
 
 	@Override
@@ -27,17 +46,14 @@ public class Day06 extends AdventOfCode{
 		// TODO Auto-generated method stub
 		
 	}
-	//fixa hashmap. streama ut första halvan av strängarna som key och den andra som value. kolla stream list to map
-	//gå igenom hela mappen och skapa nya celestial objects. räkna upp dom medans dom skapas
-	private HashMap<String> getInput() throws IOException{
-		//return Files.readAllLines(Paths.get("src/y2019/day06/Day06Input.txt"));
-		Set<String> input = new Set<>();
-		try (Stream<String> stream = Files.lines(Paths.get("src/y2019/day06/testInput"))) {
-		    stream.forEach(System.out::println);
-		}
-		Files.readAllLines(Paths.get("src/y2019/day06/testInput"));
-		return Files.readAllLines(Paths.get("src/y2019/day06/testInput"));
-	}
-	
 
+	private HashSet<String> getInput() throws IOException{
+
+		HashSet<String> input = new HashSet<>();
+		try (Stream<String> stream = Files.lines(Paths.get("src/y2019/day06/testInput"))) {
+		//try (Stream<String> stream = Files.lines(Paths.get("src/y2019/day06/Day06Input.txt"))) {
+		    stream.forEach(a -> input.add(a));
+		    return input;
+		}
+	}
 }
