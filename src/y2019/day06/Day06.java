95cfe5566ee9338bf8f922ae9b5ce512f7897761
konignik <konignik@WL358744.eu.tieto.com>
2020-02-20 08:13:09 +0100

    started day 6

--- src/y2019/day06/Day06.java
+++ src/y2019/day06/Day06.java
@@ -3,6 +3,8 @@ package y2019.day06;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Stream;
@@ -17,29 +19,29 @@ public class Day06 extends AdventOfCode{
 
 	@Override
 	public void part1() throws Exception {
+		
 		List<String> input = getInput();
-		CelestialBody com = new CelestialBody("COM");
-		String first = "COM";
-		String body = "COM";
-		for (String string : input) {
-			if(string.startsWith(first)) {
-				String[] substrings = string.split("\\)");
-				com.inOrbit.add(new CelestialBody(substrings[1]));
-				body = substrings[1];
-			}
-		}
-		input.remove(first);
-		while(!input.isEmpty()) {
-			for (String string : input) {
-				if(string.startsWith(body)) {
-					String[] substrings = string.split("\\)");
-					com.inOrbit.add(new CelestialBody(substrings[1]));
+		HashSet<String> planets = getAll(input);
+		System.out.println(planets);
+		HashMap<String,List<String>> relations = new HashMap<>();
+		int relationCounter = 0;
+		System.out.println();
+		for (String planet : planets) {
+			for (String relation : input) {
+				if(relation.startsWith(planet)) {
+					String[] substrings = relation.split("\\)");
+					if(relations.containsKey(planet)) {
+						relations.get(planet).add(substrings[1]);
+						relationCounter++;
+					}else {
+						List<String> orbitlist = new ArrayList<>();
+						relations.put(planet, orbitlist);
+					}
 				}
-			}
-			input.remove(body);
-			
+			}	
 		}
-		System.out.println(input);
+		//System.out.println(relations);
+		System.out.println("Result = " + relationCounter);
 	}
 
 	@Override
@@ -53,4 +55,20 @@ public class Day06 extends AdventOfCode{
 		return Files.readAllLines(Paths.get("src/y2019/day06/testInput"));
 		//return Files.readAllLines(Paths.get("src/y2019/day06/Day06Input.txt"));
 	}
+	
+	private HashSet<String> getAll(List<String> input){
+		HashSet<String> planets = new HashSet<>();
+		for (String string : input) {
+			String[] substrings = string.split("\\)");
+			if(!planets.contains(substrings[0])) {
+				planets.add(substrings[0]);
+			}
+			if(!planets.contains(substrings[1])) {
+				planets.add(substrings[1]);
+			}
+		}
+		
+		return planets;
+	}
+	
 }
