1062718f3ad969615f910cfcbea18225100fe759
konignik <konignik@WL358744.eu.tieto.com>
2020-09-11 16:25:35 +0200

    dfg

---
+++ src/y2019/day09/Day09.java
@@ -0,0 +1,31 @@
+package y2019.day09;
+
+import java.util.List;
+
+import adventOfCode.AdventOfCode;
+import y2019.day08.Day08;
+
+public class Day09 extends AdventOfCode{
+
+	public static void main(String[] args) {
+		new Day09().run();
+	}
+
+	@Override
+	public List<String> readInput() throws Exception {
+		return readFile("src/y2019/day09/day09Input.txt");
+	}
+
+	@Override
+	public void part1() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void part2() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
