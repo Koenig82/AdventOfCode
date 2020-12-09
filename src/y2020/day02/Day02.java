69d53434511b8ae427e57a84bbe55a53ae8e068f
konignik <konignik@WL358744.mshome.net>
2020-12-09 10:51:27 +0100

    part2 day 2

--- src/y2020/day02/Day02.java
+++ src/y2020/day02/Day02.java
@@ -38,13 +38,37 @@ public class Day02 extends AdventOfCode{
 
 	@Override
 	public void part2() throws Exception {
-		// TODO Auto-generated method stub
+		int counter = 0;
+		
+		for (String string : input) {
+			Matcher matcher = Pattern.compile("^([0-9]+)-(\\d+) (\\w): (.+)").matcher(string);
+			matcher.find();
+			
+			if(checkValidity2(Integer.parseInt(matcher.group(1)), 
+					      Integer.parseInt(matcher.group(2)),
+					      matcher.group(3).charAt(0),
+					      matcher.group(4))) {
+				counter++;
+			}
+		}
+		System.out.println(counter);
 		
 	}
 	
 	boolean checkValidity(int a,int b, char x, String input){	
 		String newstring = input.replaceAll("[^"+x+"]", "");		
 		return (newstring.length()>=a && newstring.length()<=b);
+	}
+	boolean checkValidity2(int a,int b, char x, String input){	
+		if(input.regionMatches((a-1), String.valueOf(x), 0, 1) ||
+		   input.regionMatches((b-1), String.valueOf(x), 0, 1)){
+			if(input.regionMatches((a-1), String.valueOf(x), 0, 1) &&
+			   input.regionMatches((b-1), String.valueOf(x), 0, 1)) {
+				return false;
+			}
+			return true;
+		}
+		return false;
 	}
 
 }
