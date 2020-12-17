7be132e0117bc00a7d413b1a8b4ac30ff568ab33
konignik <konignik@WL358744.eu.tieto.com>
2020-12-17 16:14:01 +0100

    day05start

---
+++ src/y2020/day05/Day05.java
@@ -0,0 +1,31 @@
+package y2020.day05;
+
+import java.util.List;
+
+import adventOfCode.AdventOfCode;
+
+public class Day05 extends AdventOfCode{
+	
+	public static void main(String[] args) {
+		new Day05().run();
+	}
+	
+	@Override
+	public List<String> readInput() throws Exception {
+		//return readFile("src/y2020/day05/Day05Input.txt");
+		return readFile("src/y2020/day05/testinput");
+	}
+
+	@Override
+	public void part1() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void part2() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
